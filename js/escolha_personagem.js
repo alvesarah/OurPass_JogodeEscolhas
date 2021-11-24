a88c8095baa29305fb41245a1b7b564edb466a83
sashamoon <sarah.alves8@hotmail.com>
2021-11-24 12:33:52 -0300

    jogo responsivo e ajustes finais

--- js/escolha_personagem.js
+++ js/escolha_personagem.js
@@ -8,20 +8,21 @@ function escolhaPersonagem() {
     button.addEventListener('click', () => {
         while(true) {
             var personagemEscolhido = prompt("Escolha seu Personagem");
-            if(personagemEscolhido == 1) {
+            if(personagemEscolhido == 1 || personagemEscolhido == "Draken" || personagemEscolhido == "draken") {
                 window.location.href = "./fases_draken/introducao_draken.html";
                 break;
             }
-            else if ( personagemEscolhido == 2) {
+            else if ( personagemEscolhido == 2 || personagemEscolhido == "Eve" || personagemEscolhido == "eve") {
                 window.location.href = "./fases_eve/introducao_eve.html";
                 break;
             }
-            else if ( personagemEscolhido == 3) {
+            else if ( personagemEscolhido == 3 || personagemEscolhido == "Ryon" || personagemEscolhido == "ryon") {
                 window.location.href = "./fases_ryon/introducao_ryon.html";
                 break;
             }
             else {
-                alert('Personagem não existe!');
+                alert('Personagem não existe! Tente escrever algo válido!');
+                break;
             }
         }
     });
