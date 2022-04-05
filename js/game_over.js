0ec74cb9cea8a9b3de89a385ae44bee0861d5eba
alvesarah <sarah.alves8@hotmail.com>
2022-04-05 16:20:56 -0300

    Correção de bug, irraa

--- js/game_over.js
+++ js/game_over.js
@@ -1,4 +1,4 @@
 function gameOver() {
-    setTimeout(function(){ window.location.href = "https://sashamoon.github.io/OurPass_JogodeEscolhas/view/index.html"; }, 2000);
+    setTimeout(function(){ window.location.href = "https://alvesarah.github.io/OurPass_JogodeEscolhas/view/index.html"; }, 2000);
 }
 gameOver();
